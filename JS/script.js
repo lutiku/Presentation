144bee0c5e1940d7a5495e1288e4bb6ae02005ed
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-09-16 23:16:19 +0200

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+function activeButton_1() {
+    document.getElementById('button_1').style.borderColor = 'white'
+}
+
+function activeButton_2() {
+    document.getElementById('button_2').style.borderColor = 'white'
+}
+function activeButton_3() {
+    document.getElementById('button_3').style.borderColor = 'white'
+}
+
+
+
+
+let tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration :750
+
+})
+
+
+tl.add({
+    targets : '.name',
+    top : '38%',
+    opacity : 1,
+    duration :4000
+
+}, '-=1600' )
+
+
+function contact() {
+    document.getElementById('one').style.backgroundColor = '#adb5bd'
+
+}
