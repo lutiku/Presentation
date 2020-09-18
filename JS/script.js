ade5eb674da10e292b202cd5fb4acb7f59dd24e8
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-09-19 00:45:54 +0200

    Update script.js : Swup

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
+
+/*Transition SWUP */
+
+
+
+const swup = new Swup({
+    plugins: [new SwupOverlayTheme()]
+});
+
+/*______*/
+
+
+
+
 function activeButton_1() {
     document.getElementById('button_1').style.borderColor = 'white'
 }
@@ -47,3 +61,48 @@ function contact() {
     document.getElementById('one').style.backgroundColor = '#adb5bd'
 
 }
+
+
+
+/*Skills*/
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'radar',
+
+    // The data for our dataset
+    data: {
+        labels: ['Javascript', 'React', 'VueJs', 'AnimeJs', '', 'UX/UI', 'back'],
+        datasets: [
+            {
+            label: "languages",
+            /*backgroundColor: 'rgb(255, 99, 132)',*/
+
+            fillColor: "rgba(43,176,212,.4)",
+            strokeColor: "rgba(43,176,212,1)",
+            pointColor: "rgba(43,176,212,1)",
+            pointHighlightStroke: "rgba(43,176,212,1)",
+
+            data: [10, 20, 30, 40, 50, 60, 70]
+             },
+
+            {
+                label: "skills",
+               /* backgroundColor: 'rgb(255, 99, 132)',*/
+
+                fillColor: "rgba(43,176,212,.4)",
+                strokeColor: "rgba(43,176,212,1)",
+                pointColor: "rgba(43,176,212,1)",
+                pointHighlightStroke: "rgba(43,176,212,1)",
+
+                data: [5, 15, 25, 35, 45, 55, 65]
+            }
+
+
+
+        ]
+    },
+
+    // Configuration options go here
+    options: {}
+});
