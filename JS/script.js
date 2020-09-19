57f711e9a8b7fb5594d2236c06ce59f04789f783
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-09-20 00:57:41 +0200

    Charts.Js script

--- JS/script.js
+++ JS/script.js
@@ -65,44 +65,55 @@ function contact() {
 
 
 /*Skills*/
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'radar',
 
-    // The data for our dataset
+
+let labels = ['Javascript', 'VueJs', 'React', 'ChartJS', 'Bootstrap', 'CSS', 'Swup', 'AnimeJS', 'ThreeJS'];
+let myChart = document.getElementById("myChart").getContext('2d');
+
+let chart = new Chart(myChart, {
+    type: 'radar',
     data: {
-        labels: ['Javascript', 'React', 'VueJs', 'AnimeJs', '', 'UX/UI', 'back'],
+        labels: labels,
         datasets: [
             {
-            label: "languages",
-            /*backgroundColor: 'rgb(255, 99, 132)',*/
-
-            fillColor: "rgba(43,176,212,.4)",
-            strokeColor: "rgba(43,176,212,1)",
-            pointColor: "rgba(43,176,212,1)",
-            pointHighlightStroke: "rgba(43,176,212,1)",
-
-            data: [10, 20, 30, 40, 50, 60, 70]
-             },
-
-            {
-                label: "skills",
-               /* backgroundColor: 'rgb(255, 99, 132)',*/
-
-                fillColor: "rgba(43,176,212,.4)",
-                strokeColor: "rgba(43,176,212,1)",
-                pointColor: "rgba(43,176,212,1)",
-                pointHighlightStroke: "rgba(43,176,212,1)",
-
-                data: [5, 15, 25, 35, 45, 55, 65]
+                label: 'Skills',
+                fill: true,
+                backgroundColor: "rgba(179, 181, 198, 0.2)",
+                borderColor: "rgba(179, 181, 198, 1)",
+                pointBorderColor: "#fff",
+                pointBackgroundColor: "rgba(179, 181, 198, 1)",
+                data: [42, 45, 35, 25, 32, 42, 38, 50, 50]
             }
-
-
-
         ]
     },
+    options: {
+        title: {
+            text: "Front",
+            display: true
+        },
 
-    // Configuration options go here
-    options: {}
+        scale: {
+            gridLines: {
+                color: 'white'/*['black', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo']*/
+            }
+        },
+
+        legend:{
+            labels:{
+                fontColor: 'white'
+
+            },
+
+            text:{
+                fontColor: 'white'
+            },
+
+        }
+    }
 });
+
+
+
+
+
+
